f9765e021d64c9813af7aca4375e8d1dd1fd1db9
Hamid <ammaraslammuhammad@gmail.com>
2023-08-18 17:29:15 +0500

    toggle addition + bookmark

--- src/components/VersesComponent.tsx
+++ src/components/VersesComponent.tsx
@@ -6,6 +6,7 @@ type Props = {
 }
 
 export default function VersesComponent({ heading, content  }: Props) {
+    // set bold property on click
     const [isBold, setIsBold] = useState(false);
       
         const handleClick = () => {
@@ -16,6 +17,7 @@ export default function VersesComponent({ heading, content  }: Props) {
           fontWeight: isBold ? 'bold' : 'normal',
         };
     return (
+        // content
         <div className="flex font-normal" style={textStyle} onClick={handleClick}>
             <p className="text-base  min-w-[65px] text-light-green ">
                 {heading}
