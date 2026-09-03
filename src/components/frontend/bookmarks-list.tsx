0aa2cc587833cf788ea2ba878f3862704176ad2f
Aqeel Haider <haideraqeel86@hotmail.com>
2026-09-03 21:20:04 +0500

    fix: trim trailing space on bookmark icon path

--- src/components/frontend/bookmarks-list.tsx
+++ src/components/frontend/bookmarks-list.tsx
@@ -105,7 +105,7 @@ export default function BookmarksList() {
                         width={15}
                         height={15}
                         alt=""
-                        src="/icons/bookfill-icon.png "
+                        src="/icons/bookfill-icon.png"
                       />
                     </button>
                   </HoverCardTrigger>
